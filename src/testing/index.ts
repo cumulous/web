a2cc1c41b3753412bbfcfa7be2b693bd3360e96a
Denis Loginov <dloginov@mit.edu>
2017-04-27 21:25:57 +0000

    Fix linting errors in testing/index.ts

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture }   from '@angular/core/testing';
-import { By }                 from '@angular/platform-browser';
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 export function selectElement<T> (fixture: ComponentFixture<T>, locator: string) {
   return fixture.debugElement.query(By.css('h1')).nativeElement;
