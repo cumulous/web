5483de38c80497f72f742de6599bdb1cf478e3f9
Denis Loginov <dloginov@mit.edu>
2017-04-27 21:16:11 +0000

    Refactor syntax for app component tests
    We create an auxiliary testing library (for now, just
    with one function to select nativeElement from a fixture).
    
    We also put all preparation into a single sync beforeEach(),
    and remove unnecessary (and async) compileComponents().
    
    As a result, our tests have become one-line lean.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,37 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { selectElement } from '../testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let h1: HTMLElement;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
+    });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    h1 = selectElement(fixture, 'h1');
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  });
+
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'app works!'`, () => {
+    expect(app.title).toEqual('app works!');
+  });
+
+  it('should render title in a h1 tag', () => {
+    expect(h1.textContent).toContain('app works!');
+  });
 });
---
+++ src/testing/index.ts
@@ -0,0 +1,6 @@
+import { ComponentFixture }   from '@angular/core/testing';
+import { By }                 from '@angular/platform-browser';
+
+export function selectElement<T> (fixture: ComponentFixture<T>, locator: string) {
+  return fixture.debugElement.query(By.css('h1')).nativeElement;
+}
