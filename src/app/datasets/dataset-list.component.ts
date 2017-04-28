e6418898842c100599088060189e3837d3b77154
Denis Loginov <dloginov@mit.edu>
2017-04-28 03:57:11 +0000

    Create Datasets module

---
+++ src/app/datasets/dataset-list.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dataset-list.component.html',
+})
+export class DatasetListComponent { }
---
+++ src/app/datasets/datasets.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { DatasetListComponent } from './dataset-list.component';
+
+@NgModule({
+  declarations: [
+    DatasetListComponent,
+  ],
+})
+export class DatasetsModule { }
