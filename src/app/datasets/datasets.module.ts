0de18da9ab4deb558871f9bf13be1cfc93a2be76
Michelle Duan <mduan@mit.edu>
2017-05-01 22:44:52 +0000

    Create SharedModule and add it to datasets module

--- src/app/datasets/datasets.module.ts
+++ src/app/datasets/datasets.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../shared/shared.module';
 import { DatasetListComponent } from './dataset-list.component';
 import { DatasetsRoutingModule } from './datasets-routing.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     DatasetsRoutingModule,
   ],
   declarations: [
