0684770b1555b488ff89f46d096e3dfec26f6e61
Michelle Duan <mduan@mit.edu>
2017-05-01 22:56:55 +0000

    Import and export ngx datatable module in shared module
    Add ngx datatable dependency in package.json

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
-
+  imports: [ NgxDatatableModule ],
+  exports: [ NgxDatatableModule ],
 })
 
 export class SharedModule { }
