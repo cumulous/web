baf18cce6552a3baf773efdb12697fc3f82c1881
Michelle Duan <mduan@mit.edu>
2017-05-02 00:04:47 +0000

    Fix element selector in spec testing module

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -2,5 +2,5 @@ import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 export function selectElement<T> (fixture: ComponentFixture<T>, locator: string) {
-  return fixture.debugElement.query(By.css('h1')).nativeElement;
+  return fixture.debugElement.query(By.css(locator)).nativeElement;
 }
