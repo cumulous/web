dc2215787a3ce11035e8ebfe58e1bc66499aedbb
Denis Loginov <dloginov@mit.edu>
2017-05-13 23:21:12 +0000

    Fix async e2e tests and shorten their syntax
    It turns out that to properly support async tests,
    we need to RETURN their output (as Promise) instead of just calling them.
    
    We now use the shorthand return syntax (() => ...), which
    also renders the steps more succinct.

--- e2e/steps/app.ts
+++ e2e/steps/app.ts
@@ -1,13 +1,12 @@
 import { $, location, locationOf, navigateTo, steps } from '../support/world';
 
-steps(({When, Then}) => {
-  When(/^I open our app$/, () => {
-    navigateTo('/');
-  });
-  Then(/^I should see a message saying that the app works$/, () => {
-    $('app-root h1').getText().should.become('app works!');
-  });
-  Then(/^I should see the "(.*)" page$/, page => {
-    location().should.become(locationOf(page));
-  });
+steps(({Given, When, Then}) => {
+  Given(/^I am on the "(.*)" page$/, page =>
+    navigateTo((locationOf(page))));
+  When(/^I open our app$/, () =>
+    navigateTo('/'));
+  Then(/^I should see a message saying that the app works$/, () =>
+    $('app-root h1').getText().should.become('app works!'));
+  Then(/^I should see the "(.*)" page$/, page =>
+    location().should.become(locationOf(page)));
 });
