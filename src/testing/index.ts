ab03f2109537eb1aecaca59a9a75666ce8aa75a1
Denis Loginov <dloginov@mit.edu>
2017-05-13 23:59:09 +0000

    Add unit test method to select multiple elements

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -4,3 +4,8 @@ import { By } from '@angular/platform-browser';
 export function selectElement<T> (fixture: ComponentFixture<T>, locator: string) {
   return fixture.debugElement.query(By.css(locator)).nativeElement;
 }
+
+export function selectElements<T> (fixture: ComponentFixture<T>, locator: string) {
+  return fixture.debugElement.queryAll(By.css(locator))
+    .map(element => element.nativeElement);
+}
