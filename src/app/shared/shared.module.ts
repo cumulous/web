03bbe9583f570986d18374d35fcfbedad28114e9
Denis Loginov <dloginov@mit.edu>
2017-06-20 22:27:11 +0000

    Export CommonModule through SharedModule
    That will enable functionality from CommonModule such as pipes.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
-  imports: [ NgxDatatableModule ],
-  exports: [ NgxDatatableModule ],
+  imports: [
+    CommonModule,
+    NgxDatatableModule,
+  ],
+  exports: [
+    CommonModule,
+    NgxDatatableModule,
+  ],
 })
 
 export class SharedModule { }
