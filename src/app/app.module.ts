3e805145f7e179ae4908e0a2d681d6cbf3a89fee
Jonathan Esteban <jesteban@mit.edu>
2017-07-28 21:02:17 +0000

    Install and import Angular Materials library to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,9 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { DatasetsModule } from './datasets/datasets.module';
 
+
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CoreModule,
     DatasetsModule,
     AppRoutingModule,
