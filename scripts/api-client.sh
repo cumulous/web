12bad01716e9427935cb6116945f51b9045b3c47
Denis Loginov <dloginov@mit.edu>
2017-08-16 00:31:28 +0000

    Modify API client script to use Cognito instead of Auth0

--- scripts/api-client.sh
+++ scripts/api-client.sh
@@ -3,7 +3,7 @@
 set -e
 
 WORKDIR="tmp"
-AUTH0_CONFIG="${WORKDIR}/.auth0.conf"
+AUTH_CONFIG="${WORKDIR}/.auth.conf"
 API_CONFIG="${WORKDIR}/.api.conf"
 SWAGGER_FILE="${WORKDIR}/swagger.json"
 SWAGGER_UI="node_modules/swagger-ui-dist/index.html"
@@ -14,24 +14,21 @@ CODEGEN_DEST="src/app/api"
 
 mkdir -p "${WORKDIR}"
 
-if [ ! -f "${AUTH0_CONFIG}" ] || [ ! -f "${API_CONFIG}" ]; then
+if [ ! -f "${AUTH_CONFIG}" ] || [ ! -f "${API_CONFIG}" ]; then
   read -p "API domain: " API_DOMAIN
-  read -p "Auth0 domain: " AUTH0_DOMAIN
-  read -p "Auth0 client ID: " AUTH0_CLIENT_ID
-  read -p "Auth0 client secret: " -s AUTH0_CLIENT_SECRET
+  read -p "Auth domain: " AUTH_DOMAIN
+  read -p "Web client ID: " WEB_CLIENT_ID
+  read -p "Command-line client ID: " CLI_CLIENT_ID
+  read -p "Command-line client secret: " -s CLI_CLIENT_SECRET
 
   echo "
     request = POST
-    url = \"https://${AUTH0_DOMAIN}/oauth/token\"
-    header = \"content-type: application/json\"
-    data = \"{\
-      \\\"client_id\\\":\\\"${AUTH0_CLIENT_ID}\\\",\
-      \\\"client_secret\\\":\\\"${AUTH0_CLIENT_SECRET}\\\",\
-      \\\"audience\\\":\\\"https://${API_DOMAIN}\\\",\
-      \\\"grant_type\\\":\\\"client_credentials\\\"\
-    }\"
+    url = \"https://${AUTH_DOMAIN}/oauth2/token\"
+    header = \"Content-Type: application/x-www-form-urlencoded\"
+    header = \"Authorization: Basic $(printf ${CLI_CLIENT_ID}:${CLI_CLIENT_SECRET} | base64 -w0)\"
+    data = "grant_type=client_credentials"
     silent = true
-  " > "${AUTH0_CONFIG}"
+  " > "${AUTH_CONFIG}"
 
   echo "
     url = "https://${API_DOMAIN}/"
@@ -43,6 +40,10 @@ if [ ! -f "${AUTH0_CONFIG}" ] || [ ! -f "${API_CONFIG}" ]; then
     export const environment = {
       production: true,
       apiRoot: 'https://${API_DOMAIN}',
+      auth: {
+        clientId: '${WEB_CLIENT_ID}',
+        domain: '${AUTH_DOMAIN}',
+      }
     };
   " | cut -c 5- > "src/environments/environment.prod.ts"
 fi
@@ -50,7 +51,7 @@ fi
 echo
 echo Authenticating...
 
-TOKEN=$(curl -K "${AUTH0_CONFIG}" | jq -r '.access_token')
+TOKEN=$(curl -K "${AUTH_CONFIG}" | jq -r '.access_token')
 curl -K "${API_CONFIG}" -H "Authorization: ${TOKEN}" > "${SWAGGER_FILE}"
 
 echo Generating API client...
