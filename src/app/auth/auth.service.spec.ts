4970db73eda664a45fc3c1ddd57dae888088d1ef
Denis Loginov <dloginov@mit.edu>
2017-08-17 23:16:54 +0000

    Implement session management in AuthService
    We store session object and retrieve its status through isAuthenticated().
    In addition, we preserve and redirect to the initial URL upon
    successful authentication.

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { CognitoAuth } from 'amazon-cognito-auth-js/dist/amazon-cognito-auth';
 
@@ -8,16 +10,29 @@ import { AuthService } from '../auth/auth.service';
 describe('AuthService', () => {
   const fakeClientID = 'client-12345';
   const fakeDomain = 'login.example.org';
+  const fakePath = '/fake/path';
 
   const fakeConfig = () => {
     return new AuthConfig(fakeClientID, fakeDomain);
   };
 
+  const fakeSession = (isValid: boolean) => {
+    const spyOnSession = jasmine.createSpyObj('session', ['isValid']);
+    spyOnSession.isValid = () => isValid;
+    return spyOnSession;
+  };
+
   let service: AuthService;
   let auth: CognitoAuth;
+  let router: Router;
+
+  let spyOnNavigate: jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
       providers: [
         AuthService,
         { provide: AuthConfig, useValue: fakeConfig() },
@@ -26,35 +41,88 @@ describe('AuthService', () => {
 
     service = TestBed.get(AuthService);
     auth = (service as any).auth;
+
+    router = TestBed.get(Router);
+    spyOnNavigate = spyOn(router, 'navigateByUrl');
   });
 
   it('constructs CognitoAuth object with correct parameters', () => {
-    expect(auth).toEqual(new CognitoAuth({
+    expect(auth).toEqual(Object.assign(new CognitoAuth({
       ClientId: fakeClientID,
       AppWebDomain: fakeDomain,
       TokenScopesArray: [],
-      RedirectUriSignIn: window.location.href,
-      RedirectUriSignOut: window.location.href,
+      RedirectUriSignIn: window.location.origin + '/login',
+      RedirectUriSignOut: window.location.origin + '/login',
+    }), {
+      userhandler: {
+        onSuccess: jasmine.any(Function),
+        onFailure: jasmine.any(Function),
+      },
     }));
   });
 
   describe('login()', () => {
+    let spyOnCallOrder: jasmine.Spy;
     let spyOnParseResponse: jasmine.Spy;
     let spyOnGetSession: jasmine.Spy;
 
     beforeEach(() => {
-      spyOnParseResponse = spyOn(auth, 'parseCognitoWebResponse');
-      spyOnGetSession = spyOn(auth, 'getSession');
+      spyOnCallOrder = jasmine.createSpy('callOrder');
+      spyOnParseResponse = spyOn(auth, 'parseCognitoWebResponse')
+        .and.callFake(() => spyOnCallOrder('parseCognitoWebResponse'));
+      spyOnGetSession = spyOn(auth, 'getSession')
+        .and.callFake(() => spyOnCallOrder('getSession'));
 
       service.login();
     });
 
-    it('login() calls auth.parseCognitoWebResponse() with correct parameters', () => {
-      expect(spyOnParseResponse).toHaveBeenCalledWith(window.location.href);
+    it('calls auth.parseCognitoWebResponse() with correct parameters first', () => {
+      expect(spyOnParseResponse).toHaveBeenCalledWith(router.url);
+      expect(spyOnParseResponse).toHaveBeenCalledTimes(1);
+      expect(spyOnCallOrder.calls.first().args[0]).toBe('parseCognitoWebResponse');
     });
 
-    it('login() calls auth.getSession() with correct parameters', () => {
-      expect(spyOnGetSession).toHaveBeenCalled();
+    it('calls auth.getSession() last', () => {
+      expect(spyOnGetSession).toHaveBeenCalledTimes(1);
+      expect(spyOnCallOrder.calls.mostRecent().args[0]).toBe('getSession');
     });
+  });
+
+  describe('sets isAuthenticated status upon', () => {
+    it('successful authentication', () => {
+      auth.userhandler.onSuccess(fakeSession(true));
+      expect(service.isAuthenticated()).toBeTruthy();
+    });
+    it('unsuccessful authentication', () => {
+      auth.userhandler.onFailure();
+      expect(service.isAuthenticated()).toBeFalsy();
+    });
+  });
+
+  describe('navigates to correct path upon successful authentication ' +
+           'if localStorage.guardedUrl was', () => {
+    let expectedPath: string;
+    afterEach(() => {
+      auth.userhandler.onSuccess(fakeSession(true));
+      expect(spyOnNavigate).toHaveBeenCalledWith(expectedPath);
+    });
+    it('non-empty', () => {
+      localStorage.setItem('guardedUrl', fakePath);
+      expectedPath = fakePath;
+    });
+    it('undefined', () => {
+      localStorage.removeItem('guardedUrl');
+      expectedPath = '/';
+    });
+    it('empty', () => {
+      localStorage.setItem('guardedUrl', '');
+      expectedPath = '/';
+    });
+  });
+
+  it('guardedUrl sets localStorage.guardedUrl to correct path', () => {
+    localStorage.removeItem('guardedUrl');
+    service.guardedUrl = fakePath;
+    expect(localStorage.getItem('guardedUrl')).toBe(fakePath);
   });
 });
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { CognitoAuth } from 'amazon-cognito-auth-js/dist/amazon-cognito-auth';
 
@@ -7,15 +8,24 @@ import { AuthConfig } from './auth.config';
 @Injectable()
 export class AuthService {
   private readonly auth: CognitoAuth;
+  private session;
 
-  constructor(config: AuthConfig) {
+  constructor(config: AuthConfig, private router: Router) {
     this.auth = new CognitoAuth({
       ClientId: config.clientId,
       AppWebDomain: config.domain,
       TokenScopesArray: [],
-      RedirectUriSignIn: window.location.href,
-      RedirectUriSignOut: window.location.href,
+      RedirectUriSignIn: this.callbackUrl,
+      RedirectUriSignOut: this.callbackUrl,
     });
+    this.auth.userhandler = {
+      onSuccess: session => this.onSuccess(session),
+      onFailure: () => this.onFailure(),
+    };
+  }
+
+  private get callbackUrl() {
+    return window.location.href.split('/').slice(0, 3).concat('login').join('/');
   }
 
   login() {
@@ -24,10 +34,31 @@ export class AuthService {
   }
 
   private parseResponse() {
-    this.auth.parseCognitoWebResponse(window.location.href);
+    this.auth.parseCognitoWebResponse(this.router.url);
   }
 
   private getSession() {
     this.auth.getSession();
+  }
+
+  isAuthenticated() {
+    return this.session && this.session.isValid();
+  }
+
+  private onSuccess(session) {
+    this.session = session;
+    this.router.navigateByUrl(this.guardedUrl);
+  }
+
+  private onFailure() {
+    this.session = null;
+  }
+
+  set guardedUrl(url: string) {
+    localStorage.setItem('guardedUrl', url);
+  }
+
+  get guardedUrl() {
+    return localStorage.getItem('guardedUrl') || '/';
   }
 }
