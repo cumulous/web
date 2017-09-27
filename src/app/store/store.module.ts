a3523663cb456215587096f8b42dffeffa3b6ac3
Denis Loginov <dloginov@mit.edu>
2017-09-27 00:21:12 +0000

    Implement StoreModule with Devtools for debugging
    This module will only be activated if
    the Redux extension is present and activated
    for the current tab.

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { StoreModule as Store } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { reducers } from './reducers';
 
 @NgModule({
   imports: [
     Store.forRoot(reducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
   ],
 })
 export class StoreModule {}
