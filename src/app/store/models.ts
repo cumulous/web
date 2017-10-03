acfd5a327729fd9ebc7d48084123469d35f52b58
Denis Loginov <dloginov@mit.edu>
2017-10-03 23:17:58 +0000

    Remove deprecated "type" from Property class in store models
    Previously, we used this field to specify
    how a property should be displayed.
    
    However, since then we inferred display type from
    the "name" (e.g. "project_id" or "created_at"),
    which obviates the need for an extra field.

--- src/app/store/models.ts
+++ src/app/store/models.ts
@@ -3,7 +3,6 @@ export class Property {
     readonly name: string,
     readonly label: string = name.charAt(0).toUpperCase() + name.substring(1),
     readonly column: boolean = true,
-    readonly type: 'string' | 'date' | 'project' = 'string',
   ) {}
 }
 
