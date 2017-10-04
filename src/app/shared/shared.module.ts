ff86416d7da8ba0a7f72dbee5c4f0bdf0a4b5475
Denis Loginov <dloginov@mit.edu>
2017-10-04 23:03:49 +0000

    Remove PipesModule
    It also needs a redesign to fit the reactive model.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ItemsModule } from './items/items.module';
-import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   exports: [
-    CommonModule,
     ItemsModule,
-    PipesModule,
   ],
 })
 export class SharedModule {}
