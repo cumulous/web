76aecf9695e67a3120dfbf4dd337bfbbf58b8b4f
Denis Loginov <dloginov@mit.edu>
2017-10-04 22:56:59 +0000

    Delete DialogModule
    The old DialogModule no longer fits in the current (reactive)
    data model. We will re-design it later on.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { DialogModule } from './dialog/dialog.module';
 import { ItemsModule } from './items/items.module';
 import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   exports: [
     CommonModule,
-    DialogModule,
     ItemsModule,
     PipesModule,
   ],
