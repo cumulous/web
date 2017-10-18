bd04daf577860577a8c7d5a18425e35ddb6c8a86
Denis Loginov <dloginov@mit.edu>
2017-10-18 01:36:32 +0000

    Move RxJS imports to a dedicated file imported in CoreModule
    This change helps keeping CoreModule uncluttered from RxJS imports.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,22 +4,14 @@ import { MatTabsModule } from '@angular/material';
 import { ApiModule } from '../api/api.module';
 import { Configuration as ApiConfig } from '../api/configuration';
 
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/first';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/withLatestFrom';
-
 import { AuthModule } from '../auth/auth.module';
 import { LoginModule } from '../login/login.module';
 import { SessionModule } from '../session/session.module';
 import { StoreModule } from '../store/store.module';
 
 import { environment } from '../../environments/environment';
+
+import './rxjs';
 
 @NgModule({
   imports: [
