083b469b889af3f2515c7ce160c74c1ebbb3bda7
Denis Loginov <dloginov@mit.edu>
2017-10-20 18:20:11 +0000

    Use a dedicated testing environment for unit tests
    Previously, we relied on environment config that
    used real values for the beta and production backend stacks.
    
    For unit tests, that is unnecessary and actually
    fails tests in CircleCI because we don't set up
    these environments for public builds.
    
    Now, we define a dedicated 'test' environment
    with fake values that mimic real ones but
    don't relate to the actual stacks.
    
    This change enables us to fully de-couple unit tests
    from the backend deployments.

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  api: {
+    baseUrl: '/api',
+  },
+  auth: {
+    clientId: '1234567890abcdef',
+    domain: 'example.auth.us-east-1.amazoncognito.com',
+    expiresIn: 4200,
+  }
+};
