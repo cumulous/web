351d6c7dcea7ec3b1483255737373c9147d63827
Denis Loginov <dloginov@mit.edu>
2017-10-25 15:17:42 +0000

    Add API models for User and Client

--- src/app/api/models/index.ts
+++ src/app/api/models/index.ts
@@ -1,6 +1,8 @@
 export { Project } from './project';
 export { Dataset } from './dataset';
 export { Analysis } from './analysis';
+export { User } from './user';
+export { Client } from './client';
 
 export interface ListResponse<Item> {
   items: Item[];
